63214870fcb9f90750d544dc03e120cc1f4cba5d
Victor de Souto <victor.souto@hotmail.com>
2022-10-25 18:27:06 -0300

    chore: add factory exercise

---
+++ exercicio/factory.js
@@ -0,0 +1,21 @@
+// Crie uma factory function de carro que recebe os seguintes parâmetros
+// Parâmetros: cor, modelo, ano, nome do dono, endereco completo do dono
+// {
+//     cor: String;
+//     modelo: String;
+//     ano: Number;
+//     nomeDono: String;
+//     enderecoCompletoDono: String;
+// }
+//
+// Retorno: {
+//     cor: String;
+//     modelo: String;
+//     ano: Number;
+//     dono: {
+//         nome: String;
+//         endereco: {
+//             rua: String;
+//             bairro: String;
+//     };
+// }
