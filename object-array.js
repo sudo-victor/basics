84933ef5e7d8ce1f22d60647e3dcf297f51d9144
Victor de Souto <victor.souto@hotmail.com>
2022-10-25 17:17:25 -0300

    chore: add object and array lesson

---
+++ object-array.js
@@ -0,0 +1,21 @@
+// Object
+
+// Crie um objeto que represente um carro
+// Deverá conter os seguintes atributos, cor, modelo, ano, darPartida(function), freiar(function)
+
+// Array
+
+// pop - remove o último item
+// shift - remove o primeiro item
+// push - adiciona na última posiçao
+// length - tamanho do array
+// slice - recorta o array e retorna o valor recortado
+// splice - recorta o array e retorna o array sem os valores recortados
+
+
+// Iteraçao
+// map - itera e retorna outro array com o mesmo tamanho
+// filter - itera e filtra o array retornando apenas os itens que passam na condiçao
+// find -  retorna o primeiro item que passa pela condiçao
+// findIndex -  retorna o index do primeiro item que passa pela condiçao
+// reduce -  itera e retorna um valor reduzido
